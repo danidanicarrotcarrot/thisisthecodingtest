36ef7f34e1cf6cd8ddcdb87c5b508fb6d69a779c
Replit user <>
2022-08-21 13:35:28 +0000

    그리디

---
+++ CHAPTER.03/3-4.py
@@ -0,0 +1,17 @@
+# 단순하게 푸는 답안 예시
+n, k = map(int, input().split())
+result = 0
+
+while n >= k:
+    while n%k != 0:
+        n -= 1
+        result += 1
+    n //= k
+    result += 1
+
+# 마지막으로 남은 수에 대해 1씩 빼기
+while n > 1:
+    n -=1
+    result += 1
+
+print(result)
